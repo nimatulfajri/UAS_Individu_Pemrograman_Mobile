044ef0b71a4bce7009f36b9a229e7c5ad26926c7
paizzt <martevenfaisal@gmail.com>
2023-12-12 22:51:47 +0800

    uas mobile individu

---
+++ app/src/main/java/com/project/myrecyclerview/Login.kt
@@ -0,0 +1,30 @@
+package com.project.myrecyclerview
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.project.myrecyclerview.databinding.ActivityLoginBinding
+
+class Login : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.loginButton.setOnClickListener(View.OnClickListener {
+            if (binding.username.text.toString() == "nimatulfajri10.03@gmail.com" && binding.password.text.toString() == "12345"){
+                Toast.makeText(this, "Login Success!", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this@Login, MainActivity::class.java))
+            } else {
+                Toast.makeText(this, "Login Failed!", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+}
